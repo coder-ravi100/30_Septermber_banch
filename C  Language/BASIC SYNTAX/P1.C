2c178ec7464bb8b23bdbf1d4ba289f89988c0ffd
coder-ravi100 <ravirathod100100@gmail.com>
2025-08-30 16:07:11 +0530

    UPDATES

---
+++ C  Language/BASIC SYNTAX/P1.C
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+/*TOPIC IN C LANGUAGE
+----------------------
+1.PRINT AND SCAN  FUNCTION
+2.OPERATORS
+3.SCOPE VARIABLE
+4.IF AND ELSE STATEMENT
+5.LOOPS
+6.ARRAY
+7.POINTER
+8.STRING
+9.FUNCTION
+10.STRUCTURE AND UNION
+11.FILES
+----------------------*/
+
